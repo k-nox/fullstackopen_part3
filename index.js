d943c88e5989ea1594effb6f2f4dcbdddcfd0b42
Lux Barker <lux.barker13@gmail.com>
2021-04-23 14:17:21 -0700

    Exercise 3.3
    Add handler for requests for individual persons

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const { response } = require('express');
 const express = require('express');
 const app = express();
@@ -35,6 +36,18 @@ app.get('/api/persons', (req, res) => {
 // GET request for /info route
 app.get('/info', (req, res) => {
   res.send(`<p>Phonebook has info for ${persons.length} people.</p> <p>${new Date()}</p>`);
+})
+
+// GET request for single person
+app.get('/api/persons/:id', (req, res) => {
+  const id = +req.params.id;
+  const person = persons.find(p => p.id === id)
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).send('Person does not exist');
+  }
 })
 
 const PORT = 3001
