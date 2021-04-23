85e75e53bdc703c0a5436684d26d1010cb8e273f
Lux Barker <lux.barker13@gmail.com>
2021-04-23 14:49:39 -0700

    Exercises 3.5 and 3.6
    Add ability to create new entries with a POST request

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ let persons = [
     name: 'Mary Poppendick',
     number: '123-789-4560'
   }
-]
+];
 
 // GET request for all persons
 app.get('/api/persons', (req, res) => {
@@ -41,7 +41,7 @@ app.get('/info', (req, res) => {
 // GET request for single person
 app.get('/api/persons/:id', (req, res) => {
   const id = +req.params.id;
-  const person = persons.find(p => p.id === id)
+  const person = persons.find(p => p.id === id);
 
   if (person) {
     res.json(person);
@@ -58,7 +58,47 @@ app.delete('/api/persons/:id', (req, res) => {
   res.status(204).end();
 })
 
-const PORT = 3001
+// generate random ID for new entries between 0 and 999999
+const generateId = () => {
+  const id = Math.floor(Math.random() * (1000000));
+
+  if (persons.some(person => person.id === id)) {
+    return generateId();
+  } else {
+    return id;
+  }
+}
+
+// POST request to create a new entry
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  if (!body.name || !body.number) {
+    return res.status(400).json({
+      error: 'name and number must both be included'
+    });
+  };
+
+  if (persons.some(person => person.name === body.name)) {
+    return res.status(400).json({
+      error: 'name must be unique'
+    });
+  };
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  res.json(person);
+})
+
+
+
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
